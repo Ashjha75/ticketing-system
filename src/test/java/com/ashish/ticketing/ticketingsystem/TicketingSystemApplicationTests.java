ac7be6d048238236d4bff6371f3bca2d615854b7
Ashish jha <ajha5645@gmail.com>
2026-04-16 23:28:39 +0530

    Initial commit

---
+++ src/test/java/com/ashish/ticketing/ticketingsystem/TicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.ticketing.ticketingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

1a290298bad1030bba7e57723065a5d51c1b5d0f
Ashish jha <ajha5645@gmail.com>
2026-04-16 23:36:29 +0530

    Add application constants and custom exception classes

---
+++ src/main/java/com/ashish/ticketing/common/entity/AuditableEntity.java
@@ -0,0 +1,23 @@
+package com.ashish.ticketing.common.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AuditableEntity extends BaseEntity {
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+}
+

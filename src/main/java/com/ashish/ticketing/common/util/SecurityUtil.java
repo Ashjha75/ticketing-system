f6a701e11f7905918b04fb3bbc71d3a6293cf719
Ashish jha <ajha5645@gmail.com>
2026-04-16 23:39:11 +0530

    initial pages

---
+++ src/main/java/com/ashish/ticketing/common/util/SecurityUtil.java
@@ -0,0 +1,33 @@
+package com.ashish.ticketing.common.util;
+
+import java.util.Optional;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public final class SecurityUtil {
+    private SecurityUtil() {
+    }
+
+    public static Optional<String> getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(authentication.getName());
+    }
+
+    public static boolean hasRole(String role) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return false;
+        }
+        for (GrantedAuthority authority : authentication.getAuthorities()) {
+            if (authority.getAuthority().equals(role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+

0cfd91940731751f4e434c02c7e9cda39e9e047a
Ashish jha <ajha5645@gmail.com>
2026-04-17 21:14:14 +0530

    updates

--- ticketing-system-ui/src/environments/environment.ts
+++ ticketing-system-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
-  adminEmails: ["admin@example.com"]
+  apiUrl: "http://localhost:8080",
+  adminEmails: ["admin@example.com"],
 };

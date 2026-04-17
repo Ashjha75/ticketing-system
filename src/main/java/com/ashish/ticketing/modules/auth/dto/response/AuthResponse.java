353f9ccfee6a481cc78bbe0602585e740e169635
Ashish jha <ajha5645@gmail.com>
2026-04-17 07:55:30 +0530

    Add auth DTOs and RefreshToken entity

---
+++ src/main/java/com/ashish/ticketing/modules/auth/dto/response/AuthResponse.java
@@ -0,0 +1,41 @@
+package com.ashish.ticketing.modules.auth.dto.response;
+
+public class AuthResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String message;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(String accessToken, String refreshToken, String message) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.message = message;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
